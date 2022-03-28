2543d01e55ce30c91db9ce8a308397b8025de18a
sezenyildirim <93969877+sezenyildirim@users.noreply.github.com>
2022-03-28 20:23:38 +0300

    Create WriterController.cs

---
+++ PresentationLayer/Controllers/WriterController.cs
@@ -0,0 +1,21 @@
+using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+
+namespace MuYuMVC.Controllers
+{
+    public class WriterController : Controller
+    {
+        WriterManager wm = new WriterManager(new EfWriterDal());
+        public ActionResult Index()
+        {
+            var WriterValues = wm.GetList();
+            return View(WriterValues);
+        }
+    }
+}
